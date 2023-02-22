7c4b8033f70fd062f9c93fadf745a7dc3262ce07
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-02-23 04:04:24 +0530

    completed permission and adding custom markers on the map for chargers

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Dimensions, TouchableOpacity, FlatList } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions, TouchableOpacity, FlatList, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import SearchBar from '../components/SearchBar';
 import Entypo from '@expo/vector-icons/Entypo';
 import { THEME_PRIMARY_COLOR, THEME_SECONDARY_COLOR } from '../constant';
 import Card from '../components/Card';
 import {chargers} from '../db.json'
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+
 
 const screenWidth = Dimensions.get('window').width;
 
+const ChargingMarkerView = ({index}) => <View style={styles.marker_view}>
+    <Text style={{fontWeight: '900', fontSize: 18}}>{index+1}</Text>
+</View>
+
 const HomeScreen = () => {
+
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+          
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+          setLocation(location);
+        })();
+    }, []);
+
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
+    console.log(text);
+    
 
     return (
         <View style={styles.container}>
@@ -25,7 +59,18 @@ const HomeScreen = () => {
                 longitude: 77.4545322,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
-            }} />
+            }} >
+                {chargers.map((marker, index) => (
+                    <Marker
+                        key={index}
+                        coordinate={{latitude: marker.latitude, longitude: marker.longitude}}
+                        title={marker.name}
+                        description={marker.address}
+                    >
+                        <ChargingMarkerView index={index} />
+                    </Marker>
+                ))}
+            </MapView>
             <View style={styles.footer_container}>
                 <FlatList
                     data={chargers}
@@ -81,5 +126,13 @@ const styles = StyleSheet.create({
         bottom: 30,
         zIndex:20,
     },
+    marker_view: {
+        width: 30, 
+        height: 30, 
+        backgroundColor: THEME_PRIMARY_COLOR, 
+        borderRadius: '50%',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 });
 
